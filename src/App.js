6182f84cb05a9ed536d19dfc4ab9108bf0bb22fd
Gustavo <gustavosohne38@gmail.com>
2021-03-02 21:54:44 -0300

    feito

--- src/App.js
+++ src/App.js
@@ -4,27 +4,76 @@ import BlackEarth from './assets/Black Earth.mp3'
 import NameLess2 from './assets/SemNome-2.mp3'
 import Sodom from './assets/Sodom.mp3'
 import LionsDen from './assets/lions den.mp3'
+import AudioPlayer from 'material-ui-audio-player'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <br />
         <h1 style={{ color: 'darkred' }}> Dominus Fatua - Metal Band</h1>
         <h3>Lista de Músicas: </h3>
 
         <h4>Black Earth</h4>
-        <ReactAudioPlayer style={{ width: '40%' }} src={BlackEarth} controls />
-        <h4>Sem Nome 2</h4>
-        <ReactAudioPlayer style={{ width: '40%' }} src={NameLess2} controls />
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={BlackEarth}
+        />
+        <br />
+        <h4>Nameless 2</h4>
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={NameLess2}
+        />
+        <br />
         <br />
         <br />
 
         <h4>Covers</h4>
         <h4>Lions Den - Morbid Angel</h4>
-        <ReactAudioPlayer style={{ width: '40%' }} src={LionsDen} controls />
-
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={LionsDen}
+        />
+        <br />
         <h4>Sodom</h4>
-        <ReactAudioPlayer style={{ width: '40%' }} src={Sodom} controls />
+
+        <AudioPlayer
+          elevation={1}
+          width="55%"
+          variation="default"
+          spacing={3}
+          download={false}
+          autoplay={false}
+          order="standart"
+          preload="auto"
+          loop={true}
+          src={Sodom}
+        />
         <br />
         <br />
       </header>
